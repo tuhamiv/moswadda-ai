563b12d0325ec376df2b8191ef3cbdb06fa531f5
Tuhami <tuhami.vc@gmail.com>
2026-06-19 15:54:51 +0300

    feat(ui): implement authentication pages with clerk components and tailwindcss styles

---
+++ src/app/pages/SignInPage.tsx
@@ -0,0 +1,11 @@
+import {SignIn} from "@clerk/react"
+
+function SignInPage() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignInPage
---
+++ src/app/pages/SignUpPage.tsx
@@ -0,0 +1,11 @@
+import {SignUp} from "@clerk/react";
+
+function SignUpPage() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <SignUp />
+        </div>
+    )
+}
+
+export default SignUpPage
